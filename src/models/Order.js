929f7dec033893f5640825a776dd2f97e5b624b6
cooleo273 <leulteferi273@gmail.com>
2024-09-26 21:06:19 +0300

    order-date

--- src/models/Order.js
+++ src/models/Order.js
@@ -5,10 +5,7 @@ const orderSchema = new mongoose.Schema({
   orderDate: { type: Date, required: true },
   customerName: { type: String, required: true }, // Customer's name
   customerPhone: { type: String, required: true }, // Customer's phone number
-  items: [{ 
-    name: String, 
-    quantity: Number // Add quantity field for each item
-  }], // Array of items ordered
+  items: [{ type: String, required: true }], // Array of items ordered
   cafeName: { type: String, required: true }, // Name of the cafe where the order was placed 
   tx_ref: { type: String, required: true, unique: true }, // Transaction reference from payment
   paymentStatus: { type: String, default: 'pending' }, // Payment status: 'pending', 'paid', 'failed',
